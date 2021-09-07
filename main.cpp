5c2c94f97f355f4ece13c67222c7d29542350adf
MaksymilianJaniec <90253603+MaksymilianJaniec@users.noreply.github.com>
2021-09-07 13:33:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	
+float kwotaBrutto;
+
+cout <<"Podaj kwotê brutto:"<<endl;
+cin >> kwotaBrutto;
+
+cout<< "Kwota podatku wynosi: \t" << kwotaBrutto*0.19<<endl;
+	
+cout<< "Kwota netto wynosi: \t" << kwotaBrutto- kwotaBrutto*0.19<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
